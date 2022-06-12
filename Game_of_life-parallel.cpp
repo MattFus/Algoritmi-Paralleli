f50c7c668e245f53bd755102b591c41318e7ccd6
Matteo <89725278+MattFus@users.noreply.github.com>
2022-06-12 16:37:53 +0200

    Update Game_of_life-parallel.cpp

--- Game_of_life-parallel.cpp
+++ Game_of_life-parallel.cpp
@@ -41,7 +41,7 @@ void exchBord(){
     MPI_Status status;
     if(myrank == 0){
         MPI_Send(&ReadM[V(1,0)], nCols, MPI_INT, 1, 0, MPI_COMM_WORLD);
-        MPI_Isend(&ReadM[V((nRows/2),0)], nCols, MPI_INT, 1, 1, MPI_COMM_WORLD, &req);
+        MPI_Send(&ReadM[V((nRows/2),0)], nCols, MPI_INT, 1, 1, MPI_COMM_WORLD);
         MPI_Recv(&ReadM[V(0,0)], nCols, MPI_INT, 1, 1, MPI_COMM_WORLD, &status);
         MPI_Recv(&ReadM[V((nRows/2)+1,0)], nCols, MPI_INT, 1, 0, MPI_COMM_WORLD, &status);
     }
